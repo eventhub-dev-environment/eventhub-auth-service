9fac74a470eb3536a264f99357a8101e81c91b0b
jpospina00 <juan.ospina.vanegas@correounivalle.edu.co>
2026-09-16 19:54:28 -0500

    Feat: handleInvalidJson

--- src/main/java/com/eventhub/auth_service/infrastructure/adapter/in/web/GlobalExceptionHandler.java
+++ src/main/java/com/eventhub/auth_service/infrastructure/adapter/in/web/GlobalExceptionHandler.java
@@ -1,14 +1,17 @@
 package com.eventhub.auth_service.infrastructure.adapter.in.web;
 
 import com.eventhub.auth_service.domain.exception.InvalidCredentialsException;
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.net.URI;
 import java.time.Instant;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -34,6 +37,23 @@ public class GlobalExceptionHandler {
         ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, "Error de validación en los campos");
         problemDetail.setTitle("Petición Inválida");
         problemDetail.setProperty("invalidFields", errors);
+        problemDetail.setProperty("timestamp", Instant.now());
+        return problemDetail;
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ProblemDetail handleInvalidJson(HttpMessageNotReadableException ex) {
+        String errorMessage = "El cuerpo de la solicitud es inválido o contiene valores no permitidos.";
+
+        if (ex.getCause() instanceof InvalidFormatException ife && ife.getTargetType().isEnum()) {
+            errorMessage = String.format("Valor no permitido en el cuerpo. Los valores válidos para %s son: %s",
+                    ife.getTargetType().getSimpleName(),
+                    Arrays.toString(ife.getTargetType().getEnumConstants()));
+        }
+
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, errorMessage);
+        problemDetail.setTitle("Lectura de JSON Fallida");
+        problemDetail.setType(URI.create("https://eventhub.com/errors/invalid-json"));
         problemDetail.setProperty("timestamp", Instant.now());
         return problemDetail;
     }
